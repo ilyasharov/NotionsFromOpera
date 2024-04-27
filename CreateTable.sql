9d09eb13305410edf0ac38641daefdae3bebe669
ilyasharov <50085011+ilyasharov@users.noreply.github.com>
2024-04-27 08:51:52 +0300

    Update SQL script

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,6 +1,6 @@
-create table Notions(
-id int primary key,
-Category varchar(50),
-DateCreated varchar (50),
-content text
-)
+CREATE TABLE NotionsTemp (
+    id int PRIMARY KEY,
+	Content text,
+    Category varchar (100),
+	dateCreated VARCHAR(100);
+);
